cb4442efa37d4a6ecdde87406c6c01bacf51a702
Alexander Emelianov <a.m.emelianov@gmail.com>
2017-12-25 21:51:04 +0500

    Port to Arduino/ESP8266

--- disas/net80211/ieee80211_crypto.c
+++
@@ -1,6 +0,0 @@
-/* 0x402505c8 */
-void ICACHE_FLASH_ATTR ieee80211_crypto_attach(struct ieee80211com *ic)
-{
-	return;
-}
-
--- disas/net80211/ieee80211_var.h
+++
@@ -1,3 +0,0 @@
-struct ieee80211com {
-	enum ieee80211_protmode ic_protmode;	/* 802.11g protection mode */ /* @12 */
-};
